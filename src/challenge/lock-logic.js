bf4ae844ac76cc1cf73009067fab4f60e34c6d7e
Doom01net <vba@colostate.edu>
2021-03-01 22:32:14 -0700

    Done with my assignment

--- src/challenge/lock-logic.js
+++ src/challenge/lock-logic.js
@@ -7,7 +7,30 @@ const lockState = window.mobx.observable({
   wheels: [0, 0, 0, 0]
 })
 
+function arrayEqual (a, b) {
+  for (let i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false
+  }
+  return true
+}
 function changeDialValue (index, incrementBy) {
+  for (let i = 0; i < lockState.wheels.length; i++) {
+    if (i == index) {
+      if (lockState.wheels[i] == 0 && (incrementBy < 0)) {
+        lockState.wheels[i] = 9
+      } else if (lockState.wheels[i] == 9 && (incrementBy > 0)) {
+        lockState.wheels[i] = 0
+      } else {
+        lockState.wheels[i] += incrementBy
+      }
+    }
+  }
+
+  if (arrayEqual(SECRET_COMBO, lockState.wheels)) {
+    lockState.locked = false
+  }
+
+  redirect('bogdan')
   // This part is missing some code
   // This function is automatically called when the user clicks on a chevron
   // it will be called with a wheel index and an amount to change the value by
